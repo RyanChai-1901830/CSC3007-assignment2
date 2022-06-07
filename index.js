e5008c583d01062f7e60812ce3172659cb9cb2c6
unknown <1901830@sit.singaporetech.edu.sg>
2022-06-07 19:47:05 +0800

    Code completed

---
+++ index.js
@@ -0,0 +1,265 @@
+// Data for singapore cases recorded for major offences 2011 - 2020
+var data2011 = [
+    { offence: 'Murder', case: 16 },
+    { offence: 'Serious Hurt', case: 406 },
+    { offence: 'Rape', case: 150 },
+    { offence: 'Outrage of Modesty', case: 1396 },
+    { offence: 'Rioting', case: 172 },
+    { offence: 'Robbery', case: 394 },
+    { offence: 'Housebreaking', case: 637 },
+    { offence: 'Theft Of Motor Vehicle', case: 673 },
+    { offence: 'Snatch Theft', case: 349 },
+    { offence: 'Cheating Related Offences', case: 3819 }
+];
+
+var data2012 = [
+    { offence: 'Murder', case: 11 },
+    { offence: 'Serious Hurt', case: 452 },
+    { offence: 'Rape', case: 133 },
+    { offence: 'Outrage of Modesty', case: 1420 },
+    { offence: 'Rioting', case: 191 },
+    { offence: 'Robbery', case: 290 },
+    { offence: 'Housebreaking', case: 545 },
+    { offence: 'Theft Of Motor Vehicle', case: 586 },
+    { offence: 'Snatch Theft', case: 286 },
+    { offence: 'Cheating Related Offences', case: 3419 }
+];
+
+var data2013 = [
+    { offence: 'Murder', case: 16 },
+    { offence: 'Serious Hurt', case: 476 },
+    { offence: 'Rape', case: 120 },
+    { offence: 'Outrage of Modesty', case: 1325 },
+    { offence: 'Rioting', case: 162 },
+    { offence: 'Robbery', case: 254 },
+    { offence: 'Housebreaking', case: 507 },
+    { offence: 'Theft Of Motor Vehicle', case: 404 },
+    { offence: 'Snatch Theft', case: 239 },
+    { offence: 'Cheating Related Offences', case: 3695 }
+];
+
+var data2014 = [
+    { offence: 'Murder', case: 14 },
+    { offence: 'Serious Hurt', case: 513 },
+    { offence: 'Rape', case: 164 },
+    { offence: 'Outrage of Modesty', case: 1372 },
+    { offence: 'Rioting', case: 168 },
+    { offence: 'Robbery', case: 230 },
+    { offence: 'Housebreaking', case: 311 },
+    { offence: 'Theft Of Motor Vehicle', case: 314 },
+    { offence: 'Snatch Theft', case: 165 },
+    { offence: 'Cheating Related Offences', case: 5465 }
+];
+
+var data2015 = [
+    { offence: 'Murder', case: 14 },
+    { offence: 'Serious Hurt', case: 473 },
+    { offence: 'Rape', case: 162 },
+    { offence: 'Outrage of Modesty', case: 1294 },
+    { offence: 'Rioting', case: 114 },
+    { offence: 'Robbery', case: 123 },
+    { offence: 'Housebreaking', case: 250 },
+    { offence: 'Theft Of Motor Vehicle', case: 302 },
+    { offence: 'Snatch Theft', case: 84 },
+    { offence: 'Cheating Related Offences', case: 8242 }
+];
+
+var data2016 = [
+    { offence: 'Murder', case: 18 },
+    { offence: 'Serious Hurt', case: 492 },
+    { offence: 'Rape', case: 149 },
+    { offence: 'Outrage of Modesty', case: 1282 },
+    { offence: 'Rioting', case: 112 },
+    { offence: 'Robbery', case: 96 },
+    { offence: 'Housebreaking', case: 212 },
+    { offence: 'Theft Of Motor Vehicle', case: 165 },
+    { offence: 'Snatch Theft', case: 63 },
+    { offence: 'Cheating Related Offences', case: 8268 }
+];
+
+var data2017 = [
+    { offence: 'Murder', case: 8 },
+    { offence: 'Serious Hurt', case: 420 },
+    { offence: 'Rape', case: 185 },
+    { offence: 'Outrage of Modesty', case: 1561 },
+    { offence: 'Rioting', case: 103 },
+    { offence: 'Robbery', case: 69 },
+    { offence: 'Housebreaking', case: 160 },
+    { offence: 'Theft Of Motor Vehicle', case: 143 },
+    { offence: 'Snatch Theft', case: 49 },
+    { offence: 'Cheating Related Offences', case: 8364 }
+];
+
+var data2018 = [
+    { offence: 'Murder', case: 9 },
+    { offence: 'Serious Hurt', case: 429 },
+    { offence: 'Rape', case: 213 },
+    { offence: 'Outrage of Modesty', case: 1728 },
+    { offence: 'Rioting', case: 107 },
+    { offence: 'Robbery', case: 72 },
+    { offence: 'Housebreaking', case: 204 },
+    { offence: 'Theft Of Motor Vehicle', case: 114 },
+    { offence: 'Snatch Theft', case: 48 },
+    { offence: 'Cheating Related Offences', case: 9193 }
+];
+
+var data2019 = [
+    { offence: 'Murder', case: 11 },
+    { offence: 'Serious Hurt', case: 373 },
+    { offence: 'Rape', case: 281 },
+    { offence: 'Outrage of Modesty', case: 1605 },
+    { offence: 'Rioting', case: 94 },
+    { offence: 'Robbery', case: 55 },
+    { offence: 'Housebreaking', case: 186 },
+    { offence: 'Theft Of Motor Vehicle', case: 85 },
+    { offence: 'Snatch Theft', case: 35 },
+    { offence: 'Cheating Related Offences', case: 12363 }
+];
+
+var data2020 = [
+    { offence: 'Murder', case: 8 },
+    { offence: 'Serious Hurt', case: 445 },
+    { offence: 'Rape', case: 348 },
+    { offence: 'Outrage of Modesty', case: 1320 },
+    { offence: 'Rioting', case: 59 },
+    { offence: 'Robbery', case: 37 },
+    { offence: 'Housebreaking', case: 164 },
+    { offence: 'Theft Of Motor Vehicle', case: 60 },
+    { offence: 'Snatch Theft', case: 18 },
+    { offence: 'Cheating Related Offences', case: 16523 }
+];
+
+// Chart properties
+// set the dimensions and margins of the graph
+var margin = { top: 30, right: 30, bottom: 70, left: 60 },
+    width = 1200 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#displaychart")
+    .append("svg")
+    .attr("viewBox", `0 0 1200 600`)
+    // .attr("width", width + margin.left + margin.right)
+    // .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+// Initialize the X axis
+var x = d3.scaleBand()
+    .domain([0, 20000])
+    .range([0, width])
+    .padding(0.2);
+var xAxis = svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+
+// Initialize the Y axis
+var y = d3.scaleLinear()
+    .domain([0, 20000])
+    .range([height, 0]);
+var yAxis = svg.append("g")
+    .attr("class", "myYaxis")
+
+// create a tooltip
+var Tooltip = d3.select("body")
+    .select("#displaychart")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("position", "absolute")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "2px")
+    .style("border-radius", "5px")
+    .style("padding", "5px");
+
+// A function that create / update the plot for a given variable:
+function display_data(data) {
+
+    var mouseover = function(d) {
+        Tooltip.style("opacity", 1);
+        d3.select(this)
+            .style("stroke", "red")
+            .style("opacity", 1)
+            .style("stroke-width", 3);
+    };
+
+    var mousemove = function(d, i) {
+        let pos = d3.select(this).node().getBoundingClientRect();
+        Tooltip.html("Total Crimes: " + i.case)
+            .style('left', `${(window.pageXOffset  + pos['x'] - 50)}px`)
+            .style('top', `${(window.pageYOffset  + pos['y'] - 50)}px`);
+        // .style("left", (parseInt(d3.select(this).attr("x")) + document.getElementById("displaychart").offsetLeft) + "px")
+        //     .style("top", (parseInt(d3.select(this).attr("y")) + document.getElementById("displaychart").offsetTop) + "px");
+        // .style("left", d3.select(this).attr("x") + "px")
+        // .style("top", d3.select(this).attr("y") + "px");
+    };
+    var mouseleave = function(d) {
+        Tooltip.style("opacity", 0);
+        d3.select(this).style("stroke", "none").style("opacity", 0.8);
+    };
+    // Update the X axis
+    x.domain(data.map(function(d) { return d.offence; }))
+    xAxis.call(d3.axisBottom(x))
+
+    // Update the Y axis
+    y.domain([0, d3.max(data, function(d) { return d.case })]);
+    yAxis.transition().duration(1000).call(d3.axisLeft(y));
+
+    var u = svg.selectAll("rect")
+        .data(data)
+
+    u
+        .enter()
+        .append("rect")
+        .merge(u)
+        .attr("x", (d) => x(d.offence)) // X axis, types of crime
+        .attr("y", (d) => y(d.case)) // Y axis, number of times crime occured
+        .attr("width", x.bandwidth())
+        .attr("height", (d) => height - y(d.case))
+        .attr("fill", "#FFBF00")
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave)
+    u
+        .exit()
+        .remove()
+
+    // svg
+    //     .selectAll("mybar")
+    //     .data(data)
+    //     .join("rect")
+    //     .attr("x", (d) => x(d.offence)) // X axis, types of crime
+    //     .attr("y", (d) => y(d.case)) // Y axis, number of times crime occured
+    //     .attr("width", x.bandwidth())
+    //     .attr("height", (d) => height - y(d.case))
+    //     .attr("fill", "#FFBF00")
+    //     .on("mouseover", mouseover)
+    //     .on("mousemove", mousemove)
+    //     .on("mouseleave", mouseleave);
+
+
+    // var u = svg.selectAll("mybar")
+    //     .data(data)
+    // u
+    //     .enter()
+    //     .append("rect")
+    //     .merge(u)
+    //     .transition()
+    //     .duration(1000)
+    //     .attr("x", function(d) { return x(d.offence); })
+    //     .attr("y", function(d) { return y(d.case); })
+    //     .attr("width", x.bandwidth())
+    //     .attr("height", function(d) { return height - y(d.case); })
+    //     .attr("fill", "#69b3a2")
+    //     .on('mouseover', tip.show)
+    //     .on('mouseout', tip.hide)
+
+    // u
+    //     .exit()
+    //     .remove()
+
+
+}
+
+display_data(data2011)
